e5cf7d5114c693189ae3f673d2e3e07bc5f1d961
nadir.alkan <nadir.alkan@obss.com.tr>
2017-11-26 12:11:55 +0300

    Test

--- src/main/java/tr/com/nadir/firstboot/FirstbootApplication.java
+++ src/main/java/tr/com/nadir/firstboot/FirstbootApplication.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,7 +13,7 @@ public class FirstbootApplication {
   public static void main(String[] args) {
     SpringApplication.run(FirstbootApplication.class, args);
   }
-  @RequestMapping(value = "/hello", method = RequestMethod.GET)
+  @RequestMapping(value = "/")
   public String hello() {
     return "Hello, server time:" + new Date();
   }

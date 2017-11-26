b34b645f7443e60ea5761f9b300eda2fd0073adf
nadir.alkan <nadir.alkan@obss.com.tr>
2017-11-26 12:04:05 +0300

    !

--- src/main/java/tr/com/nadir/firstboot/FirstbootApplication.java
+++ src/main/java/tr/com/nadir/firstboot/FirstbootApplication.java
@@ -1,12 +1,21 @@
 package tr.com.nadir.firstboot;
 
+import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstbootApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FirstbootApplication.class, args);
+  }
+  @RequestMapping(value = "/hello", method = RequestMethod.GET)
+  public String hello() {
+    return "Hello, server time:" + new Date();
+  }
 }
